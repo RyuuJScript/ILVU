4501ed81ce91577ba354f194e6448d34b3b411b0
RyuuJScript <AmityRamos2007@gmail.com>
2025-07-28 20:49:52 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const slides = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('.next');
-let current = 0;
-
-nextBtn.addEventListener('click', () => {
-  slides[current].classList.remove('active');
-  current = (current + 1) % slides.length;
-  slides[current].classList.add('active');
-});
